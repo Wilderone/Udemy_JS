0b89902442ac0743086b3a28cc7b3c38114797d2
Widerone <prins@inbox.ru>
2020-04-28 14:01:00 +0700

    new work

--- script.js
+++ script.js
@@ -1,9 +1,15 @@
-let money = prompt('Your monthly budget:')
-let time = prompt('Write date in YYYY-MM-DD format')
-let debet = prompt('Обязательный платеж')
-let value = prompt('Сумма')
-let debet2 = prompt('Обязательный платеж')
-let value2 = prompt('Сумма')
+let money, time;
+
+function start() {
+    money = +prompt('Your monthly budget:', '')
+    time = prompt('Write date in YYYY-MM-DD format', '')
+
+    while (isNaN(money) || money == '' || money == null) {
+        money = +prompt('Your monthly budget:')
+        console.log(money)
+    }
+}
+start()
 
 let appData = {
     budget: money,
@@ -11,16 +17,69 @@ let appData = {
     expenses: {},
     optionalExpenses: {},
     income: [],
-    savings: false
+    savings: true
+
+};
+
+
+function chooseExp() {
+    while (Object.keys(appData.expenses).length < 2) {
+        let a = prompt('Обязательный платеж')
+        let b = prompt('Сумма')
+        if ((typeof (a) === 'string') && (typeof (a) != null) && (typeof (a) != null
+            && a != '' && b != '' && a.length < 50)) {
+            console.log('done')
+            appData.expenses[a] = b
+        }
+    }
+
 }
-appData.expenses[debet] = +value
-appData.expenses[debet2] = +value2
-
-let sumexp = 0
-for (key in appData.expenses) {
-    sumexp += appData.expenses[key]
+function chooseOptExp() {
+    for (let i = 0; i < 3; i++) {
+        let quest = prompt('Необязательный расход:')
+        let expens = +prompt('Сумма')
+        if ((typeof (quest) == 'string') && (typeof (quest) != null) && quest != '' && !isNaN(expens)) {
+            appData.optionalExpenses[quest] = [expens];
+        } else {
+            continue;
+        }
+    }
 }
+chooseOptExp();
+
+function detectDayBudget() {
+    let sumexp = 0
+    for (key in appData.expenses) {
+        sumexp += +appData.expenses[key]
+    }
+
+    let total = ((money - sumexp) / 30).toFixed()
+
+    return total
+
+}
+chooseExp();
+alert(detectDayBudget());
 
 
-let total = ((money - sumexp) / 30)
-alert(total)
+function checkSavings() {
+    if (appData.savings) {
+        let save = +prompt('Сумма накоплений?'),
+            percent = +prompt('какой процент?')
+        appData.monthIncome = save / 100 / 12 * percent
+        alert('Доход в месяц: ' + appData.monthIncome);
+    }
+}
+checkSavings();
+
+function detectLevel() {
+    let welth = detectDayBudget();
+    if (welth <= 100) {
+        alert('Budget too low');
+    } else if (welth > 100 && welth < 2000) {
+        alert('You are a middle-class!')
+    } else if (welth > 2000) {
+        alert('You are rich!')
+    }
+}
+detectLevel();
